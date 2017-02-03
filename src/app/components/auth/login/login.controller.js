c9e7f5936d033d65b351d9ece99c50cb4baac034
trashboat182 <folieadeux182>
2017-02-03 17:48:38 -0400

    login

--- src/app/components/auth/login/login.controller.js
+++ src/app/components/auth/login/login.controller.js
@@ -8,5 +8,26 @@ function LoginController($state) {
   console.log('LoginController');
   var login = this;
   login.abc = 'abc';
+  login.email = '';
+  login.password = '';
+
+  login.checkLogin = checkLogin;
+  login.signUp = signUp;
+
+  ////////////
+
+  function checkLogin() {
+    console.log('go to');
+    console.log('email: '+login.email);
+    console.log('password: '+login.password);
+    if(login.email==='kian182' && login.password==='12345'){
+      $state.go('register');
+    }
+  }
+
+  function signUp() {
+    $state.go('register');
+  }
+
 
 }
